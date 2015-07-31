4ab25424d2daa82a3744a8d223ea14e9d80e616d
onaliugo <hello@onali-ugo.fr>
2015-07-31 09:35:36 +0200

    Remove old lib folder

--- _lib/create-dist.js
+++
@@ -1,6 +0,0 @@
-var utils = require('uo-node-utils');
-
-module.exports = function () {
-  var distName = this.opts.src.replace('.css', '-ie.css');
-  utils.create.file(distName, this.data);
-}
--- _lib/replace-stuff.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (str) {
-  this.stuffToReplace.arr.forEach(function (rule, i) {
-    str = str.replace('$' + rule.id, rule.content);
-  });
-
-  return str;
-}
